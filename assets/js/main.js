cdb0c1aaabbaceb898a65117c486360ad94d13cf
Artuto <artutogamer@gmail.com>
2019-01-19 17:54:24 -0600

    whatever

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,6 @@
-/*
-	Spectral by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+
 
 (function($) {
-
 	var	$window = $(window),
 		$body = $('body'),
 		$wrapper = $('#page-wrapper'),
@@ -25,7 +20,7 @@
 		$window.on('load', function() {
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
-			}, 5);
+			});
 		});
 
 	// Mobile?
